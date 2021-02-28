349008429fde87a4f0dc7683270bf11c2ffed9b2
Parush Arora <parusha@uci.edu>
2021-02-28 06:18:26 -0800

    Merging data into one file using the two text files

---
+++ Prediction-of-Electricity-Usage-in-USA.R
@@ -0,0 +1,45 @@
+########################################################### DATA IMPORTING 
+library("splitstackshape")
+
+df1 = read.delim("File1.txt")
+data1 = cSplit(df1, "X1392.19503.0.14", " ")
+names(data1)<- c("ID","time","electricity")
+data1 = as.matrix(data1)
+monthly1 = tapply(data1[,3],data1[,1],sum)
+monthly1 = as.data.frame(monthly1)
+m1 = as.numeric(rownames(monthly1))
+n1 = cbind(m1,monthly1)
+names(n1)<- c("ID","electricity")
+
+df2 = read.delim("File2.txt")
+data2 = cSplit(df2, "X2113.19630.0.038", " ")
+names(data2)<- c("ID","time","electricity")
+data2 = as.matrix(data2)
+monthly2 = tapply(data2[,3],data2[,1],sum)
+monthly2 = as.data.frame(monthly2)
+m2 = as.numeric(rownames(monthly2))
+n2 = cbind(m2,monthly2)
+names(n2)<- c("ID","electricity")
+
+df3 = rbind(n1,n2)
+covariates <- as.data.frame(read.csv("Smart meters Residential post-trial survey data.csv"))
+final <- merge(df3,covariates)
+
+######################################################## DATA CLEANING
+x=0
+for (i in 1:ncol(final)){
+  x[i] = sum(is.na(final[,i]))
+}
+x <- cbind(x,1:length(x))
+y <- x[x[,1]<500,] ##index of columns we want to keep
+y1 <- y[y[,1]==0,] ##index of columns with no missing values
+y2 <- y[y[,1]!=0,] ##index of columns with missing values and not dropped
+
+final1 = final[,y[,2]] ## dropping variables with too many missing values
+
+for (i in 1:nrow(y2)){
+  newdata = cbind(final[,y2[i,2]],final[,y1[,2]])
+  final[,y2[i,2]]
+  
+  
+}
