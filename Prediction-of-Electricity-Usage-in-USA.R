403f04b021b7845e0bf3b36b7174c4f6bdc525c4
Parush Arora <parusha@uci.edu>
2021-03-04 10:46:49 -0800

    final data cleaning with logit, multinomial and ols. also starting of lasso regression

--- Prediction-of-Electricity-Usage-in-USA.R
+++ Prediction-of-Electricity-Usage-in-USA.R
@@ -1,7 +1,12 @@
 ########################################################### DATA IMPORTING 
 library("splitstackshape")
 library("nnet")
-library(data.table)
+library("data.table")
+library("Hmisc")
+library("glmnet")
+library("ggplot2")
+library("reshape")
+library("gamlr")
 
 df1 = read.delim("File1.txt")
 data1 = cSplit(df1, "X1392.19503.0.14", " ")
@@ -24,19 +29,28 @@ n2 = cbind(m2,monthly2)
 names(n2)<- c("ID","electricity")
 
 df3 = rbind(n1,n2)
-covariates <- as.data.frame(read.csv("Smart meters Residential post-trial survey data.csv"))
+covariates <- as.data.frame(read.csv("Smart meters Residential pre-trial survey data.csv"))
 final <- merge(df3,covariates)
 
+for (i in 1:ncol(final)){
+  colnames(final)[i] = i
+}
+
+final[, 28][final[, 28] == 999] <- NA
+final[, 38][final[, 38] == 9999] <- 1999
+final[, 38][final[, 38] == 29] <- 1929
+final[, 38][final[, 38] == 75] <- 1975
+final[, 38][final[, 38] == 0] <- 1990
+final[, 40][final[, 40] == 999999999] <- NA
 ######################################################## DATA CLEANING
 x=0
 for (i in 1:ncol(final)){
   x[i] = sum(is.na(final[,i]))
 }
-x <- cbind(x,1:length(x))
-y <- x[x[,1]<500,] ##index of columns we want to keep
+y <- cbind(x,1:length(x))
+y = y[y[,1]<1000,] ## dropping variables with missing values more than 1000
 y1 <- y[y[,1]==0,] ##index of columns with no missing values
 y2 <- y[y[,1]!=0,] ##index of columns with missing values and not dropped
-y2 <- y2[c(-29,-31,-32),]
 
 for (i in 1:nrow(y2)){
   newdata = cbind(final[,y2[i,2]],final[,y1[,2]])
@@ -49,12 +63,11 @@ for (i in 1:nrow(y2)){
     
       if (max(newdata1[,1]) == 1){ #2
           reg = glm(yy ~., family = "binomial",newdata1)
-          pred = predict(reg,as.data.frame(newdata[(nrow(newdata)+1-y2[i,1]):(nrow(newdata)),2:ncol(newdata),]),type = "response")
+          pred = predict(reg,as.data.frame(newdata[(nrow(newdata)+1-y2[i,1]):(nrow(newdata)),2:ncol(newdata)]),type = "response")
           
               for (j in 1:length(pred)){
                 
-                  if (pred[j]>0.5){pred[i] = 1} #3
-                  else {pred [j] = 0}}#3
+                  if (pred[j]>0.5){pred[j] = 1} else {pred[j] = 0}}#3
                
        } else {     #2
           
@@ -67,11 +80,19 @@ for (i in 1:nrow(y2)){
       
       for (j in 1:length(pred)){
         
-        if (pred[j]>0.5){pred[i] = 1} #5
+        if (pred[j]>0.5){pred[j] = 1} #5
         else {pred [j] = 0}}#5
       
-      }} else {
+      }} else if (max(newdata1[,1],na.rm = T) > 10){ 
+        
+        
+        reg <- lm(yy ~., data = newdata1)
+        pred <- predict(reg, newdata = as.data.frame(newdata[(nrow(newdata)+1-y2[i,1]):(nrow(newdata)),2:ncol(newdata),]))
+        pred <- as.numeric(pred)
+       
+         } else
   
+  {
       reg <- multinom(yy ~., data = newdata1)
     pred <- predict(reg, newdata = as.data.frame(newdata[(nrow(newdata)+1-y2[i,1]):(nrow(newdata)),2:ncol(newdata),]), "probs")
     pred <- as.numeric(colnames(pred)[apply(pred,1,which.max)])
@@ -90,4 +111,51 @@ final1 = final[,y2[,2]]
 table(is.na(final1))  ## make sure there is no NA
 
 
+mydata <- cbind(final[,2],final[y2[,2]],final[y1[,2]]) ## we will be playing with 134 variables
+
+###### Lasso regression
+colnames(mydata)[1]<-"yy"
+xx = model.matrix(yy~., mydata)[,-1]
+lambdas_to_try <- 10^seq(-3, 5, length.out = 200)
+
+# Setting alpha = 0 implements ridge regression
+lasso_reg <- cv.glmnet(xx, as.matrix(mydata[,1]), alpha = 1, lambda = lambdas_to_try, standardize = TRUE)
+
+# Plot cross-validation results
+plot(lasso_reg)
+
+coef(lasso_reg)
+
+lm(final[, 2]~.,mydata)
+
+ridge_reg <- cv.glmnet(xx, as.matrix(mydata[,1]), alpha = 0, lambda = lambdas_to_try,standardize = TRUE, nfolds = 10)
+coef(ridge_reg)
+plot(ridge_reg)
+
+
+lr = lm(yy~.,mydata)
+
+
+beta=coef(ridge_reg)
+tmp <- as.data.frame(as.matrix(beta))
+tmp$coef <- row.names(tmp)
+tmp <- reshape::melt(tmp, id = "coef")
+tmp$variable <- as.numeric(gsub("s", "", tmp$variable))
+tmp$lambda <- ridge_reg$lambda[tmp$variable+1] # extract the lambda values
+
+ggplot(tmp[tmp$coef != "(Intercept)",], aes(lambda, value, color = coef, size = 3,group = 1)) + 
+  geom_line() + 
+  scale_x_log10() + 
+  xlab("Lambda (log scale)") + 
+  guides(color = guide_legend(title = ""), 
+         linetype = guide_legend(title = "")) +
+  theme_bw() + 
+  theme(legend.key.width = unit(3,"lines"))
+
+
+
+t=c()
+for (i in 1:ncol(final)){
+  t[i]=max(final[,i],na.rm = T)
+}
 
